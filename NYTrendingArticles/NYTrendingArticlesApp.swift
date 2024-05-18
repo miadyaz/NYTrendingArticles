14ce02db944d0181fabaf0ef1a93709b6ca07cf2
Miad <Miad.yz@iCloud.com>
2024-05-18 21:20:12 +0300

    Initial Commit

---
+++ NYTrendingArticles/NYTrendingArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTrendingArticlesApp.swift
+//  NYTrendingArticles
+//
+//  Created by miad yazeed on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NYTrendingArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
